7a6f312ba13606a926b95d06091a703e6144239e
Nikita <nikitasinghtomar21@gmail.com>
2025-08-26 17:31:58 +0530

    newly updated file
    B

---
+++ c4-windows-vm.tf
@@ -0,0 +1,26 @@
+# Create an azure virtual machine.
+  
+resource "azurerm_windows_virtual_machine" "mywinvm" {
+    name = "mywintestvm1"
+    location = azurerm_resource_group.testingrg.location
+    resource_group_name = azurerm_resource_group.testingrg.name
+    size = "standard_D4sv5"
+    network_interface_ids = azurerm_network_interface.my-nic.ids
+    admin_username = "testuser"
+    admin_password = azurerm_key_vault.mytestkeyvault.id
+
+    os_disk {
+      name = "OSdisk"
+      storage_account_type = "standard_LRS"
+      caching = "Read/Write"
+    }
+    source_image_reference {
+      publisher = "MicrosoftWindowsServer"
+      version = "Latest"
+      sku = "2019-datacenter"
+      offer = "WindowsServer"
+    }
+
+}
+
+
