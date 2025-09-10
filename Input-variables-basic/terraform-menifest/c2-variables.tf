5050492a836c5274e614395c0b9567ba1f0ae99d
Nikita <nikitasinghtomar21@gmail.com>
2025-09-10 15:29:11 +0530

    change variable file

--- Input-variables-basic/terraform-menifest/c2-variables.tf
+++ Input-variables-basic/terraform-menifest/c2-variables.tf
@@ -1,30 +1,30 @@
 # Create varibles
 
-resource "varibles" "business_unit" {
+ variable "business_unit" {
     description = "Business Unit Name"
     type = string
     default = "HR"  
 }
 
-resource "variables" "environment" {
+variable "environment" {
     description = "Environment Name"
     type = string
     default = "Test"
 }
 
-resource "variables" "resource_group_name" {
+variable "resource_group_name" {
     description = "Resource Group Name"
     type = string
     default = "myrg"  
 }
 
-resource "variables" "virtual_network_name" {
+variable "virtual_network_name" {
     description = "Virtual Network Name"
     type = string
     default = "myvnet"  
 }
 
-resource "variables" "resource_group_location" {
+variable "resource_group_location" {
     description = "Resource Group Location"
     type = string
     default = "west europe"
