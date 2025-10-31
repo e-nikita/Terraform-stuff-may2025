ac7daa951dac6dca59bf17dc50a84aaf0964f80a
Nikita <nikitasinghtomar21@gmail.com>
2025-10-31 17:00:22 +0530

    updated

---
+++ output-value-basics/terraform-menifest/c5-output.tf
@@ -0,0 +1,13 @@
+# output values
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value = azurerm_resource_group.rg1.name
+}
+
+output "virtual_network_name" {
+  description = "Virtual network name"
+  value = azurerm_virtual_network.myvnet.name
+  sensitive = true  # hide sensitive info. i.e when you run terraform output vnet name,
+                    # # it will provide info by hiding details.
+}
