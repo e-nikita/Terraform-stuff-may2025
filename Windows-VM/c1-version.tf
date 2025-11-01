95603cd5ea570cf53906e145953177a0c069a783
Nikita <nikitasinghtomar21@gmail.com>
2025-11-01 11:47:44 +0530

    Resolved all merge conflicts

--- Windows-VM/c1-version.tf
+++ Windows-VM/c1-version.tf
@@ -10,25 +10,12 @@ terraform {
 # Provider block
 provider "azurerm" {
     features {}
-<<<<<<< HEAD
-=======
     subscription_id = "2d00b411-2b44-479f-9fea-9b79af9b10bb"
->>>>>>> 97c399152fc9723a3a9984a239ffec8b8933e911
 }
 
 # Resource block
 # Create a resource group
-<<<<<<< HEAD
-resource "azurerm_resource_group" "myrg1" {
-    name = "rgazeusnonprod"
-    location = "East US 2"
-
-  
-
-}
-=======
 /*resource "azurerm_resource_group" "myrg1" {
     name = "rgazeusnonprod"
     location = "East US 2"
 }*/
->>>>>>> 97c399152fc9723a3a9984a239ffec8b8933e911
--- Windows-VM/c2-resourcegrp.tf
+++ Windows-VM/c2-resourcegrp.tf
@@ -2,16 +2,6 @@
 # create resource group
 resource "azurerm_resource_group" "testingrg" {
     name = "testingrg1"
-<<<<<<< HEAD
-    location = "westus" 
-}
-
-# create resource group
-resource "azurerm_resource_group" "testingrg" {
-    name = "testingrg1"
-    location = "westus" 
-=======
     location = "central india" 
->>>>>>> 97c399152fc9723a3a9984a239ffec8b8933e911
 }
 
